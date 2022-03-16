76287e68ccd1f7ac6b8e7380f95dee936b8d3381
yoti124 <91452196+yoti124@users.noreply.github.com>
2022-03-16 13:31:59 +0200

    Update app_geo.py

--- app_geo.py
+++ app_geo.py
@@ -2,7 +2,7 @@ import time
 import base64
 import streamlit as st
 import pandas as pd
-import geopy
+# import geopy
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 import plotly.express as px
